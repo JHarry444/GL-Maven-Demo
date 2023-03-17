fde7d687508e66d25e7a4033b1c0aabf5a66f381
jordan <jharrison1@qa.com>
2023-03-17 10:00:57 +0000

    init commit

---
+++ src/main/java/com/qa/demo/Runner.java
@@ -0,0 +1,12 @@
+package com.qa.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		List<Object> list = new ArrayList<>();
+	}
+
+}
